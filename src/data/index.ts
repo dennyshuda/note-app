a86949d767c04d41bf09890824f9f1b2a74a98d4
Denny Setyawan Huda <dennshgit@gmail.com>
2022-11-30 15:57:06 +0700

    feat: add data

---
+++ src/data/index.ts
@@ -0,0 +1,20 @@
+export const notesData = [
+  {
+    id: 1,
+    title: "React",
+    content:
+      "React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.",
+  },
+  {
+    id: 2,
+    title: "GrapQL",
+    content:
+      "GraphQL is a query language for APIs and a runtime for fulfilling those queries with your existing data. GraphQL provides a complete and understandable description of the data in your API, gives clients the power to ask for exactly what they need and nothing more, makes it easier to evolve APIs over time, and enables powerful developer tools.",
+  },
+  {
+    id: 3,
+    title: "NextJS",
+    content:
+      "Next.js gives you the best developer experience with all the features you need for production: hybrid static & server rendering, TypeScript support, smart bundling, route pre-fetching, and more. No config needed.",
+  },
+];
