fdfbc76650712f4d6f339ae5ca62ca8b57289076
Denny Setyawan Huda <dennshgit@gmail.com>
2022-11-30 15:58:38 +0700

    feat: add notes component

--- src/components/note.tsx
+++ src/components/note.tsx
@@ -1,8 +1,14 @@
-export function Note() {
+export interface propsNote {
+  id?: number;
+  title: string;
+  content: string;
+}
+
+export function Note(props: propsNote) {
   return (
     <div>
-      <h1>Title</h1>
-      <p>Content</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 }
