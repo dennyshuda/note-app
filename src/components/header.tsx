f396acda68b2947bc4d11b4e6e6cb0b6a57bad75
Denny Setyawan Huda <dennshgit@gmail.com>
2022-12-03 17:15:01 +0700

    feat: create header component with date

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,18 @@
-interface headerProps {
-  user: string;
-}
+import { useState } from "react";
 
-export function Header(props: headerProps) {
+export function Header() {
+  setInterval(updateTime, 1000);
+  const now = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(now);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
   return (
     <div>
-      <h1>Note App {props.user}</h1>
+      <h1>Note App {time}</h1>
     </div>
   );
 }
