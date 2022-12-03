10116aeb2a117a5abbc4af354dc9d7ab10caead6
Denny Setyawan Huda <dennshgit@gmail.com>
2022-12-03 17:13:59 +0700

    feat: create handle todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
+import React, { useState } from "react";
 import { Footer } from "./components/footer";
 import { Header } from "./components/header";
-import { Note, propsNote } from "./components/note";
-import { notesData } from "./data/index";
+import { InputField } from "./components/inputField";
+import { TodoList } from "./components/todoList";
+import { Todo } from "./model";
 
 function App() {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  function handleAdd(event: React.FormEvent) {
+    event.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo }]);
+      setTodo("");
+    }
+    console.log(todos);
+  }
   return (
     <div className="App">
-      <Header user="denny" />
-      {notesData.map((noteItem: propsNote) => {
-        return (
-          <>
-            <Note
-              key={noteItem.id}
-              title={noteItem.title}
-              content={noteItem.content}
-            />
-          </>
-        );
-      })}
+      <Header />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
       <Footer />
     </div>
   );
